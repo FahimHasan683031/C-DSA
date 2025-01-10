0c82f5244fa77ff2bc699d36a6e69051b06ba8ed
theCodelogy <thecodelogy.dev@gmail.com>
2025-01-10 19:59:04 +0600

    some pattern making practice complited

--- class4/triangle_pattern.cpp
+++ class4/triangle_pattern.cpp
@@ -51,5 +51,79 @@ int main(){
         cout << endl;
     }
 
+
+ for(int i = 0; i<n; i++){
+    for(int j = 0; j<i; j++){
+            cout << " " ;
+        }
+    for(int j = 0; j<n-i; j++){
+            cout << i+1 ;
+        }
+
+        cout << endl;
+    }
+
+
+    for(int i = 0; i<n; i++){
+
+        for(int j = 0; j<n-i-1; j++){
+            cout << " " ;
+        }
+        for(int j = 0; j<i+1; j++){
+            cout << j+1 ;
+        }
+        for(int j = i; j>=1; j--){
+            cout << j ;
+        }
+
+
+        cout << endl;
+    }
+
+
+     for(int i = 0; i<n; i++){
+
+        for(int j = 0; j<n-i-1; j++){
+            cout << " " ;
+        }
+
+        cout <<"*";
+
+        if(i != 0){
+            for(int j = 0; j<2*i-1; j++){
+            cout << " " ;
+             }
+            cout <<"*";
+        }
+        cout << endl;
+    }
+
+for(int i = 0; i<n-1; i++){
+
+        for(int j = 0; j<i+1; j++){
+            cout << " " ;
+        }
+
+        cout <<"*";
+
+        if(i != n-2){
+            for(int j = 0; j<2*(n-i)-5; j++){
+            cout << " " ;
+             }
+            cout <<"*";
+        }
+        cout << endl;
+    }
+
+
+
+    for(int i = 0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
     return 0;
 }
